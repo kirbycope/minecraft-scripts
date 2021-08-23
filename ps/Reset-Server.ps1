ae2a3a92bfab72f55a0c9bd98e30369921859b90
Tim Cope <kirbycope@gmail.com>
2021-08-22 18:34:09 -0800

    Create Reset-Server.ps1

---
+++ ps/Reset-Server.ps1
@@ -0,0 +1,11 @@
+$serverPath = "C:\Server";
+cd $serverPath;
+New-Item "$serverpath\eula.txt" -Force;
+Set-Content "$serverpath\eula.txt" "eula=true";
+$serverUrl = "https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar";
+Invoke-WebRequest $serverUrl -OutFile "$serverPath\server.jar";
+#$serverProperties = "C:\Server\server.properties";
+#$find = "motd=A Minecraft Server";
+#$replace = "motd=$env:UserName's Server";
+#(Get-Content $serverProperties).replace($find, $replace) | Set-Content $serverProperties;
+java -Xmx1024M -Xms1024M -jar "$serverPath\server.jar";
