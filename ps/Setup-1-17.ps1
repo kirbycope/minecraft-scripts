5d0271977edbb85539ec6b8a0d865dcf22ccc119
Tim Cope <kirbycope@gmail.com>
2021-11-07 11:43:31 -0800

    Updates

--- ps/Setup-1-17.ps1
+++ ps/Setup-1-17.ps1
@@ -12,6 +12,18 @@ if (!(test-path $resourcepacks)) { New-Item -ItemType Directory -Force -Path $re
 if (!(test-path $saves)) { New-Item -ItemType Directory -Force -Path $saves }
 if (!(test-path $shaderpacks)) { New-Item -ItemType Directory -Force -Path $shaderpacks }
 
+# Mods - Controllable
+Remove-Item "$mods\controllable-0.13.4-1.17.1.jar" -Force -ErrorAction SilentlyContinue;
+Invoke-WebRequest "https://github.com/kirbycope/minecraft-scripts/raw/main/1.17/controllable-0.13.4-1.17.1.jar" -OutFile "$mods\controllable-0.13.4-1.17.1.jar";
+
+# Mods - Journey Map
+Remove-Item "$mods\journeymap-1.17.1-5.7.3.jar" -Force -ErrorAction SilentlyContinue;
+Invoke-WebRequest "https://github.com/kirbycope/minecraft-scripts/raw/main/1.17/journeymap-1.17.1-5.7.3.jar" -OutFile "$mods\journeymap-1.17.1-5.7.3.jar";
+
+# Mods - Optifine
+Remove-Item "$mods\OptiFine_1.17.1_HD_U_H1.jar" -Force -ErrorAction SilentlyContinue;
+Invoke-WebRequest "https://github.com/kirbycope/minecraft-scripts/raw/main/1.17/OptiFine_1.17.1_HD_U_H1.jar" -OutFile "$mods\OptiFine_1.17.1_HD_U_H1.jar";
+
 # Resource Packs - Faithful
 Remove-Item "$resourcepacks\faithful-1.17" -Force -Recurse -ErrorAction SilentlyContinue;
 Invoke-WebRequest "https://github.com/kirbycope/minecraft-scripts/raw/main/1.17/faithful-1.17.zip" -OutFile "$downloads\faithful-1.17.zip";
@@ -19,10 +31,10 @@ Expand-Archive -LiteralPath "$downloads\faithful-1.17.zip" -DestinationPath "$re
 Remove-Item "$downloads\faithful-1.17.zip" -Force -ErrorAction SilentlyContinue;
 
 # Resource Pack - Mine Brick
-Remove-Item "$resourcepacks\MineBricks-V6-128x" -Force -Recurse -ErrorAction SilentlyContinue;
-Invoke-WebRequest "https://github.com/kirbycope/minecraft-scripts/raw/main/1.17/MineBricks-V6-128x.zip" -OutFile "$downloads\MineBricks-V6-128x.zip";
-Expand-Archive -LiteralPath "$downloads\MineBricks-V6-128x.zip" -DestinationPath "$resourcepacks\MineBricks-V6-128x";
-Remove-Item "$downloads\MineBricks-V6-128x.zip" -Force -ErrorAction SilentlyContinue;
+Remove-Item "$resourcepacks\MineBricks-V7-128x" -Force -Recurse -ErrorAction SilentlyContinue;
+Invoke-WebRequest "https://github.com/kirbycope/minecraft-scripts/raw/main/1.17/MineBricks V7 128x (1.17).zip" -OutFile "$downloadsMineBricks V7 128x (1.17).zip";
+Expand-Archive -LiteralPath "$downloads\MineBricks V7 128x (1.17)MineBricks V7 128x (1.17).zip" -DestinationPath "$resourcepacks\MineBricks-V7-128x";
+Remove-Item "$downloads\MineBricks V7 128x (1.17).zip" -Force -ErrorAction SilentlyContinue;
 
 # Resource Pack - Xray
 Remove-Item "$resourcepacks\Xray_Ultimate_1.17_v4.1.2" -Force -Recurse -ErrorAction SilentlyContinue;
@@ -42,8 +54,7 @@ Invoke-WebRequest "https://github.com/kirbycope/minecraft-scripts/raw/main/1.17/
 Expand-Archive -LiteralPath "$downloads\Skyblock 4.09.1 (Hardcore).zip" -DestinationPath "$saves";
 Remove-Item "$downloads\Skyblock 4.09.1 (Hardcore).zip" -Force -ErrorAction SilentlyContinue;
 
-# Mod - Optifine
-Remove-Item "$mods\OptiFine_1.17.1_HD_U_G9.jar" -Force -ErrorAction SilentlyContinue;
-Invoke-WebRequest "https://github.com/kirbycope/minecraft-scripts/raw/main/1.17/OptiFine_1.17.1_HD_U_G9.jar" -OutFile "$downloads\OptiFine_1.17.1_HD_U_G9.jar";
-java -jar "$downloads\OptiFine_1.17.1_HD_U_G9.jar";
-Remove-Item "$downloads\OptiFine_1.17.1_HD_U_G9.jar" -Force -ErrorAction SilentlyContinue;
+# Mod Loaders - Forge
+Invoke-WebRequest "https://github.com/kirbycope/minecraft-scripts/raw/main/1.1/forge-1.17.1-37.0.104-installer.jar" -OutFile "$downloads\forge-1.17.1-37.0.104-installer";
+java -jar "$downloads\forge-1.17.1-37.0.104-installer.jar";
+Remove-Item "$downloadsforge-1.17.1-37.0.104-installer.jar" -Force -ErrorAction SilentlyContinue;
