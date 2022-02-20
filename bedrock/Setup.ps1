08abd7adf8cd8ea710f9e74725c85b248bf2d391
Timothy Cope <kirbycope@gmail.com>
2022-02-20 12:17:22 -0800

    Create Setup.ps1

---
+++ bedrock/Setup.ps1
@@ -0,0 +1,19 @@
+﻿#Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+
+$downloads = "$HOME\Downloads";
+$comMojang = "$env:LOCALAPPDATA\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang";
+$worldTemplates = "$comMojang\world_templates";
+
+# World Templates - Earth
+$fileName = "Earth";
+Remove-Item "$worldTemplates\$fileName" -Force -Recurse -ErrorAction SilentlyContinue;
+Invoke-WebRequest "https://github.com/kirbycope/Bedrock-Earth/raw/main/Bedrock-Earth.mctemplate" -OutFile "$downloads\$fileName.zip";
+Expand-Archive -LiteralPath "$downloads\$fileName.zip" -DestinationPath "$worldTemplates\$fileName";
+Remove-Item "$downloads\$fileName" -Force -ErrorAction SilentlyContinue;
+
+# World Templates - SkyBlock
+$fileName = "SkyBlock";
+Remove-Item "$worldTemplates\$fileName" -Force -Recurse -ErrorAction SilentlyContinue;
+Invoke-WebRequest "https://github.com/kirbycope/SkyBlock-Bedrock/raw/main/SkyBlock.mctemplate" -OutFile "$downloads\$fileName.zip";
+Expand-Archive -LiteralPath "$downloads\$fileName.zip" -DestinationPath "$worldTemplates\$fileName";
+Remove-Item "$downloads\$fileName" -Force -ErrorAction SilentlyContinue;
