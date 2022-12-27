98b49a48151825f13c3e47fe98742cddde7420c8
Timothy Cope <kirbycope@gmail.com>
2022-12-26 20:40:14 -0800

    Create Setup-Kirbycope.ps1

---
+++ ps/Setup-Kirbycope.ps1
@@ -0,0 +1,53 @@
+﻿#Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+
+$downloads = "$HOME\Downloads";
+
+# BEDROCK
+$comMojang = "$env:LOCALAPPDATA\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang";
+$worldTemplates = "$comMojang\world_templates";
+
+# World Template: Bedrock-Earth
+$fileName = "Bedrock-Earth";
+Remove-Item "$worldTemplates\$fileName" -Force -Recurse -ErrorAction SilentlyContinue;
+Invoke-WebRequest "https://github.com/kirbycope/$fileName/raw/main/Bedrock-Earth.mctemplate" -OutFile "$downloads\$fileName.zip";
+Expand-Archive -LiteralPath "$downloads\$fileName.zip" -DestinationPath "$worldTemplates\$fileName";
+Remove-Item "$downloads\$fileName" -Force -ErrorAction SilentlyContinue;
+
+# World Template: SkyBlock-Bedrock
+$fileName = "SkyBlock-Bedrock";
+Remove-Item "$worldTemplates\$fileName" -Force -Recurse -ErrorAction SilentlyContinue;
+Invoke-WebRequest "https://github.com/kirbycope/$fileName/raw/main/SkyBlock.mctemplate" -OutFile "$downloads\$fileName.zip";
+Expand-Archive -LiteralPath "$downloads\$fileName.zip" -DestinationPath "$worldTemplates\$fileName";
+Remove-Item "$downloads\$fileName" -Force -ErrorAction SilentlyContinue;
+
+# JAVA
+$gameDirectory = "$env:APPDATA\.minecraft";
+$saves = "$gameDirectory\saves";
+
+# Save: CopeCraft
+$fileName = "CopeCraft";
+Remove-Item "$saves\$fileName-main" -Force -Recurse -ErrorAction SilentlyContinue;
+Invoke-WebRequest "https://github.com/kirbycope/$fileName/archive/refs/heads/main.zip" -OutFile "$downloads\$fileName.zip";
+Expand-Archive -LiteralPath "$downloads\$fileName.zip" -DestinationPath "$saves";
+Remove-Item "$downloads\$fileName.zip" -Force -ErrorAction SilentlyContinue;
+
+# Save: minecRAFT
+$fileName = "minecRAFT";
+Remove-Item "$saves\$fileName-main" -Force -Recurse -ErrorAction SilentlyContinue;
+Invoke-WebRequest "https://github.com/kirbycope/$fileName/archive/refs/heads/main.zip" -OutFile "$downloads\$fileName.zip";
+Expand-Archive -LiteralPath "$downloads\$fileName.zip" -DestinationPath "$saves";
+Remove-Item "$downloads\$fileName.zip" -Force -ErrorAction SilentlyContinue;
+
+# Save: SkyBlock
+$fileName = "SkyBlock";
+Remove-Item "$saves\$fileName-main" -Force -Recurse -ErrorAction SilentlyContinue;
+Invoke-WebRequest "https://github.com/kirbycope/$fileName/archive/refs/heads/main.zip" -OutFile "$downloads\$fileName.zip";
+Expand-Archive -LiteralPath "$downloads\$fileName.zip" -DestinationPath "$saves";
+Remove-Item "$downloads\$fileName.zip" -Force -ErrorAction SilentlyContinue;
+
+# Save: world-of-minecraft
+$fileName = "world-of-minecraft";
+Remove-Item "$saves\$fileName-main" -Force -Recurse -ErrorAction SilentlyContinue;
+Invoke-WebRequest "https://github.com/kirbycope/$fileName/archive/refs/heads/main.zip" -OutFile "$downloads\$fileName.zip";
+Expand-Archive -LiteralPath "$downloads\$fileName.zip" -DestinationPath "$saves";
+Remove-Item "$downloads\$fileName.zip" -Force -ErrorAction SilentlyContinue;
