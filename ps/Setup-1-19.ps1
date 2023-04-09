af4931eb4315a8b0cdd0a17504db229e91c96813
Timothy Cope <kirbycope@gmail.com>
2023-04-09 12:02:46 -0700

    1.19.4

--- ps/Setup-1-19.ps1
+++ ps/Setup-1-19.ps1
@@ -14,17 +14,17 @@ if (!(test-path $resourcepacks)) { New-Item -ItemType Directory -Force -Path $re
 if (!(test-path $saves)) { New-Item -ItemType Directory -Force -Path $saves }
 
 # Mods - Controllable
-$fileName = "controllable-0.17.0-1.19.3";
+$fileName = "controllable-0.18.0-1.19.4";
 Remove-Item "$mods\$fileName.jar" -Force -ErrorAction SilentlyContinue;
 Invoke-WebRequest "$remote/$fileName.jar" -OutFile "$mods\$fileName.jar";
 
 # Mods - Optifine
-$fileName = "preview_OptiFine_1.19.3_HD_U_I2_pre5";
+$fileName = "OptiFine_1.19.4_HD_U_I4";
 Remove-Item "$mods\$fileName.jar" -Force -ErrorAction SilentlyContinue;
 Invoke-WebRequest "$remote/$fileName.jar" -OutFile "$mods\$fileName.jar";
 
 # Resource Packs - Faithful
-$fileName = "Faithful 32x - 1.19.3";
+$fileName = "Faithful 32x - 1.19.4";
 Remove-Item "$resourcepacks\$fileName" -Force -Recurse -ErrorAction SilentlyContinue;
 Invoke-WebRequest "$remote/$fileName.zip" -OutFile "$downloads\$fileName.zip";
 Expand-Archive -LiteralPath "$downloads\$fileName.zip" -DestinationPath "$resourcepacks\$fileName";
@@ -38,7 +38,7 @@ Expand-Archive -LiteralPath "$downloads\$fileName.zip" -DestinationPath "$saves"
 Remove-Item "$downloads\$fileName.zip" -Force -ErrorAction SilentlyContinue;
 
 # Mod Loaders - Forge
-$fileName = "forge-1.19.3-44.1.17-installer";
+$fileName = "forge-1.19.4-45.0.43-installer";
 Invoke-WebRequest "$remote/$fileName.jar" -OutFile "$downloads\$fileName.jar";
 java -jar "$downloads\$fileName.jar";
 Remove-Item "$downloads\$fileName.jar" -Force -ErrorAction SilentlyContinue;
